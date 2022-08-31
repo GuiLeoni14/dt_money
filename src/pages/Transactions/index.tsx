720f46c26d001fe8776da2c0a13606d3206e3b8d
GuiLeoni14 <guileone167@gmail.com>
2022-08-30 22:21:34 -0300

    criando tabela

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,34 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import * as S from './styles';
 
 export function PageTransactions() {
     return (
         <div>
             <Header />
             <Summary />
+            <S.Container>
+                <S.TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td style={{ width: '40%' }}>Desenvolvimento de site</td>
+                            <td>
+                                <S.PriceHighlight variant="income">R$ 12.000,00</S.PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td style={{ width: '40%' }}>Alimentação</td>
+                            <td>
+                                <S.PriceHighlight variant="outcome">- R$ 12.000,00</S.PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                    </tbody>
+                </S.TransactionsTable>
+            </S.Container>
         </div>
     );
 }
