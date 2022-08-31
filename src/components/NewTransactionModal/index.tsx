8cd14c2a8df169949235f87cd14501767b7b188f
GuiLeoni14 <guileone167@gmail.com>
2022-08-31 20:17:15 -0300

    adicionando json-server

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
 import * as S from './styles';
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from 'phosphor-react';
+import * as RadioGroup from '@radix-ui/react-radio-group';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
 export function NewTransactionModal() {
     return (
@@ -15,6 +16,14 @@ export function NewTransactionModal() {
                         <input type="text" placeholder="Descrição" required />
                         <input type="number" placeholder="Preço" required />
                         <input type="text" placeholder="Categoria" required />
+                        <S.TransactionType>
+                            <S.TransactionTypeButton variant="income" value="income">
+                                <ArrowCircleUp size={24} /> Entrada
+                            </S.TransactionTypeButton>
+                            <S.TransactionTypeButton variant="outcome" value="outcome">
+                                <ArrowCircleDown size={24} /> Saída
+                            </S.TransactionTypeButton>
+                        </S.TransactionType>
                         <button type="submit">Cadastrar</button>
                     </form>
                 </S.Content>
